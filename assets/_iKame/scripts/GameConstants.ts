cbc64553c3dcd4b08eb250e919c0b07a5c409da0
ChuongPT <chuongpt@ikameglobal.com>
2026-02-25 16:02:07 +0700

    build level PinkHaus

--- assets/_iKame/scripts/GameConstants.ts
+++ assets/_iKame/scripts/GameConstants.ts
@@ -16,8 +16,8 @@ export const STICKER = {
     AFTER_PEEL_DURATION: 0.2,
     AFTER_PEEL_WORLD_POSITION_OFFSET: new Vec3(-0.4, 1, 0),
 
-    IN_BOX_SCALE: new Vec3(0.362, 0.362, 0.362),
-    IN_CACHE_SCALE: new Vec3(0.4, 0.4, 0.4),
+    IN_BOX_SCALE: new Vec3(0.3, 0.3, 0.3),
+    IN_CACHE_SCALE: new Vec3(0.314, 0.314, 0.314),
     IN_BOX_ROTATION: new Vec3(0, 180, 0),
 
     TRANSFER_DURATION: 0.5,
--- assets/_iKame/scripts/configData/LevelDataSO.ts
+++ assets/_iKame/scripts/configData/LevelDataSO.ts
@@ -1,4 +1,4 @@
-import { _decorator, CCInteger, CCString, Prefab, RealCurve } from 'cc';
+import { _decorator, CCFloat, CCInteger, CCString, Prefab, RealCurve } from 'cc';
 import { bh } from 'db://scriptable-asset/scriptable_runtime';
 import { ColorDistributionConfig } from './ColorDistributionConfig';
 const { ccclass, property } = _decorator;
@@ -9,12 +9,13 @@ export class LevelDataSO extends bh.ScriptableAsset {
     @property(Prefab) public levelPrefab: Prefab | null = null;
     @property(CCInteger) public maxBox: number = 4;
     @property(CCInteger) public maxCache: number = 5;
+    @property(CCFloat) public curveScale : number = 1;
     @property(RealCurve) public diffCurve: RealCurve = new RealCurve();
     @property(CCString) public stickerTutName: string = "";
     @property(ColorDistributionConfig) public colorDistributionConfig: ColorDistributionConfig | null = null;
 
     public evaluateDifficulty(t: number): number {
-        return this.diffCurve.evaluate(t);
+        return this.diffCurve.evaluate(t) * this.curveScale;
     }
 }
 
